eea575862db01c52a5df3a95edc061d12e5fb430
Jorge Bautista <jorgebautista@gmail.com>
2021-02-26 00:13:19 -0500

    started github branch

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,70 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRepoRequest(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "golang introduction",
+		Description: "A golang repository",
+		Homepage:    "http://github.com",
+		Private:     true,
+		HasIssues:   false,
+		HasProjects: true,
+		HasWiki:     false,
+	}
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+	assert.EqualValues(t, request.Name, target.Name)
+	assert.EqualValues(t, request.Description, target.Description)
+	assert.EqualValues(t, request.Homepage, target.Homepage)
+	assert.EqualValues(t, request.Private, target.Private)
+	assert.EqualValues(t, request.HasIssues, target.HasIssues)
+	assert.EqualValues(t, request.HasProjects, target.HasProjects)
+	assert.EqualValues(t, request.HasWiki, target.HasWiki)
+}
+
+func TestCreateRepoResponse(t *testing.T) {
+	response := CreateRepoResponse{
+		ID:       "123",
+		Name:     "Test response",
+		FullName: "This is the full name",
+		Owner: RepoOwner{
+			ID:      22522,
+			Login:   "jebo87",
+			Url:     "http://test.com",
+			HtmlUrl: "http://another.com",
+		},
+		Permissions: RepoPermissions{
+			IsAdmin: true,
+			HasPull: true,
+			HasPush: true,
+		},
+	}
+
+	bytes, err := json.Marshal(response)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+	fmt.Println(string(bytes))
+
+	var target CreateRepoResponse
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+	fmt.Println(err)
+
+	assert.EqualValues(t, response.FullName, target.FullName)
+	assert.EqualValues(t, response.Owner.ID, target.Owner.ID)
+	assert.EqualValues(t, response.Permissions.IsAdmin, target.Permissions.IsAdmin)
+
+}
---
+++ src/api/domain/github/providers/github_provider/github_provider.go
@@ -0,0 +1,70 @@
+package github_provider
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/jebo87/golang-microservices/src/api/domain/github"
+	"github.com/jebo87/golang-microservices/src/api/domain/github/clients/restclient"
+)
+
+const (
+	headerAuthorization       = "Authorization"
+	headerAuthorizationFormat = "token %s"
+	urlCreateRepo             = "https://api.github.com/user/repos"
+)
+
+func getAuthorizationHeader(accesToken string) string {
+	return fmt.Sprintf(headerAuthorizationFormat, accesToken)
+}
+
+func CreateRepo(accessToken string, request github.CreateRepoRequest) (*github.CreateRepoResponse, *github.GithubErrorResponse) {
+	headers := http.Header{}
+	headers.Set(headerAuthorization, getAuthorizationHeader(accessToken))
+
+	response, err := restclient.Post(urlCreateRepo, request, headers)
+	if err != nil {
+		log.Println("error trying to create new github repo: %s", err)
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(fmt.Sprintf("error trying to create new github repo: %s", err))
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "invalid response body",
+		}
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode > 299 {
+		var errResponse github.GithubErrorResponse
+		if err := json.Unmarshal(bytes, &errResponse); err != nil {
+			return nil, &github.GithubErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "invalid response body",
+			}
+		}
+		errResponse.StatusCode = response.StatusCode
+		return nil, &errResponse
+	}
+
+	var result github.CreateRepoResponse
+
+	if err := json.Unmarshal(bytes, &result); err != nil {
+		log.Println(fmt.Sprintf("Error when trying to unmarshal body succesful response %s", err))
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "Error when trying to unmarshal body succesful response",
+		}
+	}
+
+	return &result, nil
+}
